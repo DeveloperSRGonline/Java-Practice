bc62ea97388bd39b00a41e19b61c0a8d5a793eb8
Shivam Garade <shivamgarade05@gmail.com>
2026-03-14 13:24:30 +0530

    MultiDimensionalArray

---
+++ Arrays/MultiDimensionalArray.java
@@ -0,0 +1,29 @@
+
+//Java Program to demonstrate multi-dimensional arrays
+public class MultiDimensionalArray {
+
+    public static void main(String[] args) {
+        int[][] myNumbers = {{1, 2, 3, 4}, {5, 6, 7}};
+        // System.out.println(myNumbers[1][2]);
+
+        myNumbers[1][2] = 9;
+        // System.out.println(myNumbers[1][2]);
+
+        for(int i=0;i<myNumbers.length;i++)
+        {
+            for(int j=0;j<myNumbers[i].length;j++)
+            {
+                System.err.println(myNumbers[i][j]);
+            }
+        }
+
+        for(int[] row:myNumbers)
+        {
+            for(int i:row)
+            {
+                System.err.println(i);
+            }
+        }
+
+    }
+}
